923c4b95130ab53196750b77a966289179bd8a5d
Visaalini <Visaalini@Visaalini>
2026-06-25 13:45:54 +0530

    Third code Updated

---
+++ src/test/java/HandlingMouseAction/LearningMouseAction.java
@@ -0,0 +1,54 @@
+package HandlingMouseAction;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class LearningMouseAction {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20l));
+		driver.get("https://demowebshop.tricentis.com/");
+		
+		//1.Step to perform mouse operation
+		//1.create an object
+		  Actions actionobj = new Actions(driver);
+		  
+		  WebElement ele1 = driver.findElement(By.xpath("//ul[@class='top-menu']//a[contains(text(),'Computers')]"));
+		  
+		  
+		 //2.Using ref call by using movetoElement()method
+		  actionobj.moveToElement(ele1).perform();
+		  
+		  WebElement ele2 = driver.findElement(By.xpath("//ul[@class='top-menu']//a[contains(text(),'Desktops')]"));
+		  
+		  actionobj.click(ele2).perform();
+		  
+		 // actionobj.moveToElement(ele2).click(ele2).perform();
+		  
+		  //actionobj.moveToElement(ele2).doubleClick().perform();
+		  
+		  actionobj.moveToElement(ele2).contextClick(ele2).perform();
+		 
+		  
+
+		  
+		  
+		  
+		  
+		  
+				  
+				  
+				  
+		  
+		  
+	}
+
+}
